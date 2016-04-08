aaf7bc0822cf71f4e48fc4cdfff68c30335d756d
liusy182 <liusy182@gmail.com>
2016-04-08 22:35:54 +0800

    asset and initial layout

--- weather-app/PrettyWeatherViewController.swift
+++ weather-app/PrettyWeatherViewController.swift
@@ -7,18 +7,51 @@
 //
 
 import UIKit
+import Cartography
 
 class PrettyWeatherViewController: UIViewController {
-
+    private let backgroundView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.redColor()
+        setup()
+        layoutView()
+        style()
+        render(UIImage(named: "DefaultImage"))
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: setup
+    func setup(){
+        backgroundView.contentMode = .ScaleAspectFill
+        backgroundView.clipsToBounds = true
+        view.addSubview(backgroundView)
+    }
+    
+    // MARK: layout
+    func layoutView() {
+        constrain(backgroundView) { view in
+            view.top == view.superview!.top
+            view.bottom == view.superview!.bottom
+            view.left == view.superview!.left
+            view.right == view.superview!.right
+        }
+    }
+    
+    // MARK: Render
+    func render(image: UIImage?){
+        if let image = image {
+            backgroundView.image = image
+        }
+    }
+    
+    // MARK: style
+    func style(){
     }
 
 
